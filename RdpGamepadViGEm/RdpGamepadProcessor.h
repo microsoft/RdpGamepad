ce1e8e6cd513cb5fb2c82dc6d8f7d711c2329b87
JP Flouret <jflouret@microsoft.com>
2021-09-30 10:40:06 -0700

    Added connection state tool tip to notify icon

--- RdpGamepadViGEm/RdpGamepadProcessor.h
+++ RdpGamepadViGEm/RdpGamepadProcessor.h
@@ -14,25 +14,29 @@ class ViGEmTarget360;
 class RdpGamepadProcessor
 {
 public:
-	RdpGamepadProcessor();
+	explicit RdpGamepadProcessor(std::function<void()> ConnectionStateCallback);
 	~RdpGamepadProcessor();
 
 	void Start();
 	void Stop();
+	bool IsConnected() const;
 
 private:
+	std::function<void()> mConnectionStateCallback;
 	std::unique_ptr<RdpGamepad::RdpGamepadVirtualChannel> mRdpGamepadChannel;
 	std::shared_ptr<ViGEmClient> mViGEmClient;
 	std::shared_ptr<ViGEmTarget360> mViGEmTarget360;
 	std::thread mThread;
-	std::mutex mMutex;
+	mutable std::mutex mMutex;
 	unsigned int mRdpGamepadOpenRetry = 0;
 	unsigned int mRdpGamepadPollTicks = 0;
 	unsigned int mLastGetStateResponseTicks = 0;
 	bool mRdpGamepadConnected = false;
 	bool mKeepRunning = false;
+	bool mConnectionStateCallbackPending = false;
 
 	void Run();
 	void RdpGamepadTidy();
 	void RdpGamepadProcess();
+	void TriggerConnectionStateCallback();
 };
